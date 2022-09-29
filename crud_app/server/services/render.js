8620887f91e2abf823f647c8869e085ed95a63f2
deepanshu20001001027 <114352721+deepanshu20001001027@users.noreply.github.com>
2022-09-29 18:39:11 +0530

    Final Commit

---
+++ crud_app/server/services/render.js
@@ -0,0 +1,30 @@
+const axios = require('axios');
+
+let axiosConfig = {
+      headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          "Access-Control-Allow-Origin": "*",
+      }
+    };
+
+exports.homeRoutes = (req,res)=>{
+      //Make a get request to /api/users
+      axios.get('http://localhost:3000/api/users')
+      .then(function(response){
+            console.log(response)
+            res.render('index',{users:response.data});
+      })
+      .catch(err=>{
+            res.send(err);
+      })
+      
+}
+
+exports.add_user = (req,res)=>{
+      res.render('add_user')
+}
+
+exports.update_user = (req,res)=>{
+      res.render('update_user')
+}
+
